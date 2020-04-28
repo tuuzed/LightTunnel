fb60b6230163729752dddd73195e3a5bb8083bc9
tuuzed <liuyonghui.job@gmail.com>
2020-04-28 13:01:14 +0800

    更新代码

--- core/server/src/main/kotlin/lighttunnel/server/TunnelServerChannelHandler.kt
+++ core/server/src/main/kotlin/lighttunnel/server/TunnelServerChannelHandler.kt
@@ -111,12 +111,8 @@ class TunnelServerChannelHandler(
     private fun doHandleTransferMessage(ctx: ChannelHandlerContext, msg: ProtoMessage) {
         logger.trace("doHandleTransferMessage# {}, {}", ctx, msg)
         val sessionChannels = ctx.channel().attr(AttributeKeys.AK_SESSION_CHANNELS).get() ?: return
-        val sessionChannel = when (sessionChannels.tunnelRequest.type) {
-            TunnelRequest.Type.TCP -> tcpRegistry?.getSessionChannel(msg.tunnelId, msg.sessionId)
-            TunnelRequest.Type.HTTP -> httpRegistry?.getSessionChannel(msg.tunnelId, msg.sessionId)
-            TunnelRequest.Type.HTTPS -> httpsRegistry?.getSessionChannel(msg.tunnelId, msg.sessionId)
-            else -> null
-        }
+        val sessionChannel = sessionChannels.getChannel(msg.sessionId)
+        logger.trace("doHandleTransferMessage# sessionChannel={}", sessionChannel)
         sessionChannel?.writeAndFlush(Unpooled.wrappedBuffer(msg.data))
     }
 
