79f85aa1ab0e6468ae2781d8502b2c85fa196955
tuuzed <liuyonghui.job@gmail.com>
2020-10-20 15:25:35 +0800

    IncIds避免0值

--- core/base/src/main/kotlin/lighttunnel/internal/base/util/IncIds.kt
+++ core/base/src/main/kotlin/lighttunnel/internal/base/util/IncIds.kt
@@ -4,5 +4,6 @@ import java.util.concurrent.atomic.AtomicLong
 
 class IncIds {
     private val id = AtomicLong(Long.MIN_VALUE)
-    val nextId get() = id.incrementAndGet()
+    val nextId get() = incrementAndGetId.let { if (it == 0L) incrementAndGetId else it }
+    private val incrementAndGetId get() = id.incrementAndGet()
 }
