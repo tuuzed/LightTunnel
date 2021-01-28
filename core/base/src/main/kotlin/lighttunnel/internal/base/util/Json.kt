05607a9f9867e50590d2e60f0943b17b3a2f2c1e
tuuzed <liuyonghui.job@gmail.com>
2021-01-28 22:11:38 +0800

    优化代码

--- core/base/src/main/kotlin/lighttunnel/internal/base/util/Json.kt
+++
@@ -1,9 +0,0 @@
-@file:JvmName("-JsonKt")
-
-package lighttunnel.internal.base.util
-
-import org.json.JSONObject
-
-inline fun <reified T> JSONObject.getOrNull(key: String): T? = if (this.has(key)) this.get(key).let { if (it is T) it else null } else null
-
-inline fun <reified T> JSONObject.getOrDefault(key: String, defValue: T): T = if (this.has(key)) this.get(key).let { if (it is T) it else defValue } else defValue
