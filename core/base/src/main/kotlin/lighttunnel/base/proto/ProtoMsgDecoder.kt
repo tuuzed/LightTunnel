9bcaf5290e38ba68aaea65b2f847892d910b1616
tuuzed <liuyonghui.job@gmail.com>
2022-01-19 20:40:02 +0800

    优化`ProtoMsgDecoder`

--- core/base/src/main/kotlin/lighttunnel/base/proto/ProtoMsgDecoder.kt
+++ core/base/src/main/kotlin/lighttunnel/base/proto/ProtoMsgDecoder.kt
@@ -5,42 +5,29 @@ import io.netty.channel.ChannelHandlerContext
 import io.netty.handler.codec.LengthFieldBasedFrameDecoder
 
 class ProtoMsgDecoder : LengthFieldBasedFrameDecoder(
-    4 * 1024 * 1024,
+    1024 * 1024,
     0,
-    PROTO_MESSAGE_LENGTH_FIELD_LENGTH,
-    0,
-    0,
-    true
+    4
 ) {
 
-    companion object {
-        private const val MIN_BYTES = PROTO_MESSAGE_LENGTH_FIELD_LENGTH +
-            PROTO_MESSAGE_TYPE_LENGTH +
-            PROTO_MESSAGE_HEAD_LENGTH_FIELD_LENGTH
-    }
-
     @Throws(Exception::class)
-    override fun decode(ctx: ChannelHandlerContext?, input: ByteBuf?): Any? {
-        when (val rst = super.decode(ctx, input)) {
-            is ByteBuf -> {
-                if (rst.readableBytes() < MIN_BYTES) return null
-                val totalLength = rst.readInt()
-                if (rst.readableBytes() < totalLength) return null
-                // 开始解码数据
-                val type = ProtoMsgType.codeOf(rst.readByte())
-                val headLength = rst.readInt()
-                val head = ByteArray(headLength)
-                rst.readBytes(head)
-                val dataLength = totalLength -
-                    PROTO_MESSAGE_TYPE_LENGTH -
-                    PROTO_MESSAGE_HEAD_LENGTH_FIELD_LENGTH -
-                    headLength
-                val data = ByteArray(dataLength)
-                rst.readBytes(data)
-                return ProtoMsg.newInstance(type, head, data)
-            }
-            else -> return rst
+    override fun decode(ctx: ChannelHandlerContext?, `in`: ByteBuf?): Any? {
+        val frame = super.decode(ctx, `in`)
+        if (frame is ByteBuf) {
+            val totalLength = frame.readInt()
+            val type = ProtoMsgType.codeOf(frame.readByte())
+            val headLength = frame.readInt()
+            val head = ByteArray(headLength)
+            frame.readBytes(head)
+            val dataLength = totalLength -
+                PROTO_MESSAGE_TYPE_LENGTH -
+                PROTO_MESSAGE_HEAD_LENGTH_FIELD_LENGTH -
+                headLength
+            val data = ByteArray(dataLength)
+            frame.readBytes(data)
+            return ProtoMsg.newInstance(type, head, data)
         }
+        return frame
     }
 
 }
