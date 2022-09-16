bb38cf8f4f6a175a9519a431beabce1eb8d1d239
liuyonghui <liuyonghui.job@gmail.com>
2022-09-16 21:55:18 +0800

    ID: 优化代码 DESC:

--- common/src/main/kotlin/krp/common/utils/json.kt
+++ common/src/main/kotlin/krp/common/utils/json.kt
@@ -5,7 +5,7 @@ package krp.common.utils
 import org.json.JSONObject
 
 inline fun <reified T> JSONObject.getOrNull(key: String): T? =
-    if (this.has(key)) this.get(key).let { if (it is T) it else null } else null
+    if (this.has(key)) this.opt(key).let { if (it is T) it else null } else null
 
 inline fun <reified T> JSONObject.getOrDefault(key: String, defValue: T): T =
-    if (this.has(key)) this.get(key).let { if (it is T) it else defValue } else defValue
+    if (this.has(key)) this.opt(key).let { if (it is T) it else defValue } else defValue
